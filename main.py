5869b584c306743414b5d600586f5786cab2b299
Kalecio Pereira <kalecio.santana@gmail.com>
2021-07-06 11:04:32 -0300

    feat(model): create fake db and Post model

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import Optional, Text
+from datetime import datetime
 
 app = FastAPI()
+
+postdb = []
+
+class Post(BaseModel):
+    id: int
+    title: str
+    author: str
+    content: Text
+    created_at: datetime = datetime.now()
+    published_at: datetime
+    published: Optional[bool] = False
 
 @app.get("/")
 async def root():
