d8cc900b0a5918f03191bc04b34763550ac1e9df
Kalecio Pereira <kalecio.santana@gmail.com>
2021-07-06 11:54:37 -0300

    feat: remove items route and create blog route

--- main.py
+++ main.py
@@ -20,6 +20,6 @@ class Post(BaseModel):
 async def root():
     return {"message": "Hello World"}
 
-@app.get("/items/{item_id}")
-async def read_item(item_id: int):
-    return {"item_id": item_id}
+@app.get("/blog")
+async def get_posts():
+    return postdb
